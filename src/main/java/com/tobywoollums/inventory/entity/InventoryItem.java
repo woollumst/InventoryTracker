225b58ac04d3a49b16dcd4c99db43dabb54193ee
Toby <woollumst@gmail.com>
2025-06-06 22:08:54 -0700

    Update InventoryItem.java

--- src/main/java/com/tobywoollums/inventory/entity/InventoryItem.java
+++ src/main/java/com/tobywoollums/inventory/entity/InventoryItem.java
@@ -13,13 +13,26 @@ public class InventoryItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank
+    @Column(nullable = false, length = 100)
     private String name;
 
+    @Column(length = 500)
     private String description;
 
+    @Column(name = "image_url")
     private String imageUrl;
 
+    @Min(0)
     private int stockQuantity;
 
+    @Min(0)
     private int minStockQuantity; // Minimum Stock Threshold
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
